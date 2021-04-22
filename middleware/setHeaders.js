fe6a66d280d6499602831061bad4b69ff6b88429
Gbolahan Balogun <gbolahanbalogun5@gmail.com>
2021-04-22 09:34:48 +0100

    Add DELETE http method to allowed methods in access control header

--- middleware/setHeaders.js
+++ middleware/setHeaders.js
@@ -2,7 +2,10 @@ const setHeaders = (req, res, next) => {
   res.setHeader("Content-Type", "application/json");
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+  );
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
