ff7bbef4b00be621669bbf90b94510fcc90966c0
Gbolahan Balogun <gbolahanbalogun5@gmail.com>
2021-06-12 14:13:49 +0100

    Fix null issue in error handler

--- utils/getHTTPStatus.js
+++ utils/getHTTPStatus.js
@@ -1,8 +1,8 @@
 exports.getHTTPStatus = (data, successCode, failureCode) => {
   //return unauthorized status if private
-  if (data.privateErr) return 403;
+  if (data?.privateErr) return 403;
 
-  if (data.errors || data.error) {
-    return data.error === "Internal server error" ? 500 : failureCode;
+  if (data?.errors || data?.error) {
+    return data?.error === "Internal server error" ? 500 : failureCode;
   } else return successCode;
 };
